25e62b69cfb59a15faf2cfe800dea34176d0d306
FB <fbparis@gmail.com>
2012-07-12 15:08:53 +0200

    Comments and cleaning

--- scraper.class.php
+++ scraper.class.php
@@ -3,12 +3,11 @@
 /*
 	Generic scraping class by fbparis@gmail.com
 	Should be ultra fast, permanently running N simultaneous connections on N' interfaces
-
-	-Implement your own code to discover and scrap urls and use register_success_function('your_function')
 	
-		Tip: 	Maybe you want to avoid endless loops: do not add the same url more than once
-		Tip: 	You can use simple_html_dom class to handle complex scraps
-				Can be found here: http://sourceforge.net/projects/simplehtmldom/
+	Nothing to change, just call it, register your success_function and or redirect_function and run!
+	
+	See README and comments
+
 */
 
 set_time_limit(0);
@@ -25,9 +24,9 @@ class Scraper {
 	public $max_memory_usage_ratio = 0.8; // if $todo_file is set, new urls will be written in file if memory usage exceeds this ratio
 	
 	/* Public advanced stuff */
-	public $success_codes = array(200);
-	public $fail_codes = array(0,118,204,300,310,500,501,502,503,504,505,507,509);
-	public $redirect_codes = array(301,302,303,307);
+	public $success_codes = array(200); // on which http response codes we have a successful scrap
+	public $fail_codes = array(0,118,204,300,310,500,501,502,503,504,505,507,509); // on which http response codes we want to retry later
+	public $redirect_codes = array(301,302,303,307); // on which http response code we have an url redirection
 	
 	/* Private internal stuff */
 	protected $basefile = '';
@@ -54,8 +53,177 @@ class Scraper {
 	/* Special stuff for recovering mode */
 	public $recovery_mode = false;
 	protected $fp_in_offset = 0;
+			
+	/* PUBLIC API */
+	
+	/* Add an interface */
+	public function add_interface($ip=0,$user_agent=null,$max_conns=null,$auto_adjust_speed=null,$max_sleep_delay=null,$timeout=null,$proxy=null,$proxy_userpwd=null) {
+		$key = $proxy ? "$ip-$proxy" : $ip;
+		if (array_key_exists($key,$this->conns)) return false;
+		if ($user_agent === null) $user_agent = $this->default_user_agent;
+		if ($max_conns === null) $max_conns = $this->default_max_conns;
+		if ($auto_adjust_speed === null) $auto_adjust_speed = $this->default_auto_adjust_speed;
+		if ($max_sleep_delay === null) $max_sleep_delay = $this->default_max_sleep_delay;
+		if ($timeout === null) $timeout = $this->default_timeout;
+		if ($this->interfaces[$key] = new ScraperInterface($ip,$user_agent,$max_conns,$auto_adjust_speed,$max_sleep_delay,$timeout,$proxy,$proxy_userpwd)) return true;
+		return false;
+	}
+	
+	/* Add an URL to scrap */
+	public function add_url($url,$headers=null) {
+		if ($this->recovery_mode) return true;
+		if (array_key_exists($url,$this->todo)) return false;
+		$headers = is_array($headers) ? $headers : null;
+		if (is_resource($this->fp_todo) && (memory_get_peak_usage(true) / $this->memory_limit > $this->max_memory_usage_ratio)) {
+			if (@fputs($this->fp_todo,sprintf("%s %s\n",$url,serialize($headers)))) return true;
+		}
+		$this->todo[$url] = 0;
+		$this->headers[$url] = $headers;
+		return true;
+	}
+	
+	/* Remove an URL from the todo list */
+	public function remove_url($url) {
+		if (array_key_exists($url,$this->todo)) {
+			unset($this->todo[$url]);
+			unset($this->headers[$url]);
+		}
+	}
+	
+	/* 
+		Register your custom function to be called on success
 		
-	function __construct() {
+		This function must take 4 parameters:
+		-$S is a reference "to this class", $this
+		-$html is the content of the document retrieved
+		-$headers is the headers received when loading document
+		-$info is the result of curl_get_info()
+		
+		...and return a mixed value:
+		-if a single value, it will be json_encoded and saved in {SCRIPT_PATHNAME}.output (one line)
+		-if an array, each element of the array will be json_encoded and saved in {SCRIPT_PATHNAME}.output
+		
+		Warning: if the element you want to record is an array, make it an object or insert it in another array...
+		
+		Exemple function:
+		
+		function success_function($S,$html,$headers,$info) {
+			// find new urls to scrap and add them to the todo list
+			...
+			$S->add_url($url);
+			...
+			// extract data from $html
+			...
+			foreach ($matches as $data) $results[] = $data;
+			...
+			return $results;
+		}
+	*/
+	public function register_success_function($function) {
+		if (function_exists($function)) {
+			$this->success_function = $function;
+			return true;
+		}
+		$this->debug("Success function $function not found!",2);
+		return false;
+	}
+
+	/* 
+		Register your custom function to be called on redirect
+		
+		This function must take 3 parameters:
+		-$S is a reference "to this class", $this
+		-$headers is the headers received when loading document
+		-$info is the result of curl_get_info()
+		
+		...and nothing.
+		
+		Exemple function:
+		
+		function redirect_function($S,$headers,$info) {
+			// You may want to add the redirect url to the todo list:
+			...
+			if ($url = $info['redirect_url']) $S->add_url($url);
+		}
+	*/
+	public function register_redirect_function($function) {
+		if (function_exists($function)) {
+			$this->redirect_function = $function;
+			return true;
+		}
+		$this->debug("Redirect function $function not found!",2);
+		return false;
+	}
+	
+	/* Call this method to start scraping */
+	public function run() {
+		/* If no interfaces have been added, add a default one with the current IP */
+		if (!count($this->interfaces)) $this->add_interface();
+		/* Recording the total time is fun */
+		$this->timer = $this->recovery_mode ? microtime(true) - $this->timer : microtime(true);
+		if ($this->fp_in = @fopen($this->input_file,'rb')) {
+			@fseek($this->fp_in,$this->fp_in_offset);
+		} else {
+			if (file_exists($this->input_file)) {
+				$this->debug(sprintf('Input file "%s" not readable! Exiting',$this->input_file),2);
+				exit;
+			}
+		}
+		if (!$this->fp_out = @fopen($this->output_file,$this->recovery_mode ? 'ab' : 'wb')) {
+			$this->debug(sprintf('Output file "%s" not writable! Exiting',$this->output_file),2);
+			exit;
+		}
+		if (!$this->fp_errors = @fopen($this->errors_file,$this->recovery_mode ? 'ab' : 'wb')) {
+			$this->debug(sprintf('Errors file "%s" not writable! Exiting',$this->errors_file),2);
+			exit;
+		}
+		if (!$this->fp_todo = @fopen($this->todo_file,$this->recovery_mode ? 'ab' : 'wb')) {
+			$this->debug(sprintf('Todo file "%s" not writable! Exiting',$this->todo_file),2);
+			exit;
+		}
+		$mh = curl_multi_init();
+		/* $done has to be false now */
+		$this->done = count($this->interfaces) == 0;
+		/* No more need for $recovery_mode at this point */
+		$this->recovery_mode = false;
+		while (!$this->done) {
+			while ($this->assign_conn($mh)); 
+			$status = curl_multi_exec($mh,$active);
+			while ($info = curl_multi_info_read($mh)) $this->exec_conn($mh,$info['handle']);
+			usleep(50);	// save your CPU
+		}
+		curl_multi_close($mh);
+		clearstatcache();
+		if (is_resource($this->fp_todo)) @fclose($this->fp_todo);
+		if (filesize($this->todo_file) == 0) unlink($this->todo_file); // cleaning a little
+		if (is_resource($this->fp_errors)) @fclose($this->fp_errors);
+		if (filesize($this->errors_file) == 0) unlink($this->errors_file); // cleaning a little
+		if (is_resource($this->fp_out)) @fclose($this->fp_out);
+		if (filesize($this->output_file) == 0) {
+			$this->debug('No output generated',1);
+			unlink($this->output_file); // cleaning a little
+		}
+		if (is_resource($this->fp_in)) @fclose($this->fp_in);
+		$this->debug(sprintf('Done in %.3f seconds',microtime(true)-$this->timer),1);
+	}
+	
+	/* Just print some text */
+	public function debug($msg,$level=0) {
+		if ($level >= $this->debug_level) printf("%s\n",$msg);
+	}
+	
+	/* END OF PUBLIC PART */
+	
+	/* Constructor: no params */
+	public function __construct() {
+		/*
+			Calling script path and name is the prefix to several files used by the class: 
+			- recovery file: {SCRIPT_PATHNAME}.recover.inc
+			- input file: {SCRIPT_PATHNAME}.input
+			- output file: {SCRIPT_PATHNAME}.output
+			- errors file: {SCRIPT_PATHNAME}.errors
+			- todo file: {SCRIPT_PATHNAME}.todo
+		*/
 		$this->recovery_file = $_SERVER['PATH_TRANSLATED'] . '.recover.inc';
 		if (file_exists($this->recovery_file)) {
 			$this->debug('Running in recovery mode',1);
@@ -82,20 +250,25 @@ class Scraper {
 		} 
 	}
 	
-	function _destruct() {
+	/* Pseudo-Destructor: called on shutdown (normal termination or fatal error as well) */
+	public function _destruct() {
 		if (!$this->done) {
+			$this->debug('Backing up internal state...',1);
 			if (is_resource($this->fp_out)) @fclose($this->fp_out);
 			if (is_resource($this->fp_errors)) @fclose($this->fp_errors);
 			if (is_resource($this->fp_todo)) @fclose($this->fp_todo);
 			$this->recovery_mode = true;
 			$this->success_function = null;
 			$this->redirect_function = null;
+			/* No time to properly close all running connections so we mark them as ready */
 			foreach ($this->todo as $url=>$status) if ($status > 0) {
 				$this->todo[$url] = 1 - $status;
 			}
 			$this->interfaces = array();
 			$this->conns = array();
+			/* We'll keep the timer running on relaunch */
 			$this->timer = microtime(true) - $this->timer;
+			/* Also keep trace of the pointer position in the input file */
 			if (is_resource($this->fp_in)) {
 				$this->fp_in_offset = ftell($this->fp_in);
 				@fclose($this->fp_in);
@@ -105,101 +278,8 @@ class Scraper {
 			$this->done = true; // prevent multiple executions...
 		}
 	}
-	
-	public function add_interface($ip=0,$user_agent=null,$max_conns=null,$auto_adjust_speed=null,$max_sleep_delay=null,$timeout=null,$proxy=null,$proxy_userpwd=null) {
-		$key = $proxy ? "$ip-$proxy" : $ip;
-		if (array_key_exists($key,$this->conns)) return false;
-		if ($user_agent === null) $user_agent = $this->default_user_agent;
-		if ($max_conns === null) $max_conns = $this->default_max_conns;
-		if ($auto_adjust_speed === null) $auto_adjust_speed = $this->default_auto_adjust_speed;
-		if ($max_sleep_delay === null) $max_sleep_delay = $this->default_max_sleep_delay;
-		if ($timeout === null) $timeout = $this->default_timeout;
-		if ($this->interfaces[$key] = new ScraperInterface($ip,$user_agent,$max_conns,$auto_adjust_speed,$max_sleep_delay,$timeout,$proxy,$proxy_userpwd)) return true;
-		return false;
-	}
-	
-	public function add_url($url,$headers=null) {
-		if ($this->recovery_mode) return true;
-		if (array_key_exists($url,$this->todo)) return false;
-		$headers = is_array($headers) ? $headers : null;
-		if (is_resource($this->fp_todo) && (memory_get_peak_usage(true) / $this->memory_limit > $this->max_memory_usage_ratio)) {
-			if (@fputs($this->fp_todo,sprintf("%s %s\n",$url,serialize($headers)))) return true;
-		}
-		$this->todo[$url] = 0;
-		$this->headers[$url] = $headers;
-		return true;
-	}
-	
-	public function remove_url($url) {
-		if (array_key_exists($url,$this->todo)) {
-			unset($this->todo[$url]);
-			unset($this->headers[$url]);
-		}
-	}
-	
-	public function register_success_function($function) {
-		if (function_exists($function)) {
-			$this->success_function = $function;
-			return true;
-		}
-		$this->debug("Success function $function not found!",2);
-		return false;
-	}
 
-	public function register_redirect_function($function) {
-		if (function_exists($function)) {
-			$this->redirect_function = $function;
-			return true;
-		}
-		$this->debug("Redirect function $function not found!",2);
-		return false;
-	}
-	
-	/* Call this method to start scraping */
-	public function run() {
-		if (!count($this->interfaces)) $this->add_interface();
-		$this->timer = $this->recovery_mode ? microtime(true) - $this->timer : microtime(true);
-		if ($this->fp_in = @fopen($this->input_file,'rb')) {
-			@fseek($this->fp_in,$this->fp_in_offset);
-		} else {
-			if (file_exists($this->input_file)) {
-				$this->debug(sprintf('Input file "%s" not readable! Exiting',$this->input_file),2);
-				exit;
-			}
-		}
-		if (!$this->fp_out = @fopen($this->output_file,$this->recovery_mode ? 'ab' : 'wb')) {
-			$this->debug(sprintf('Output file "%s" not writable! Exiting',$this->output_file),2);
-			exit;
-		}
-		if (!$this->fp_errors = @fopen($this->errors_file,$this->recovery_mode ? 'ab' : 'wb')) {
-			$this->debug(sprintf('Errors file "%s" not writable! Exiting',$this->errors_file),2);
-			exit;
-		}
-		if (!$this->fp_todo = @fopen($this->todo_file,$this->recovery_mode ? 'ab' : 'wb')) {
-			$this->debug(sprintf('Todo file "%s" not writable! Exiting',$this->todo_file),2);
-			exit;
-		}
-		$mh = curl_multi_init();
-		$this->done = count($this->interfaces) == 0;
-		$this->recovery_mode = false;
-		while (!$this->done) {
-			while ($this->assign_conn($mh)); 
-			$status = curl_multi_exec($mh,$active);
-			while ($info = curl_multi_info_read($mh)) $this->exec_conn($mh,$info['handle']);
-			usleep(50);			
-		}
-		curl_multi_close($mh);
-		if (is_resource($this->fp_todo)) @fclose($this->fp_todo);
-		if (is_resource($this->fp_errors)) @fclose($this->fp_errors);
-		if (is_resource($this->fp_out)) @fclose($this->fp_out);
-		if (is_resource($this->fp_in)) @fclose($this->fp_in);
-		$this->debug(sprintf('Done in %.3f seconds',microtime(true)-$this->timer),1);
-	}
-	
-	protected function debug($msg,$level=0) {
-		if ($level >= $this->debug_level) printf("%s\n",$msg);
-	}
-	
+	/* Find an URL to scrap */
 	protected function assign_conn(&$mh) {
 		/* First, look in $todo for a ready to scrap url */
 		foreach ($this->todo as $url=>$status) if ($status <= 0) return $this->add_conn($mh,$url,$this->headers[$url],$status);
@@ -239,6 +319,7 @@ class Scraper {
 		return false;
 	}
 	
+	/* Find an available connexion to scrap an URL */
 	protected function add_conn(&$mh,$url,$headers=null,$status=0) {
 		foreach ($this->interfaces as $k=>$interface) if ($interface->ready()) {
 			// Last used interface is moved to the bottom of the pile 
@@ -266,6 +347,7 @@ class Scraper {
 		return false;
 	}
 	
+	/* Get a response status according to a http response code */
 	protected function get_status($http_code) {
 		if (in_array($http_code,$this->success_codes)) return 'success';
 		if (in_array($http_code,$this->fail_codes)) return 'fail';
@@ -273,6 +355,7 @@ class Scraper {
 		return null;
 	}
 	
+	/* Process a http response */
 	protected function exec_conn(&$mh,&$ch) {
 		$info = curl_getinfo($ch);
 		$url = $info['url'];
@@ -286,8 +369,8 @@ class Scraper {
 		$status = $this->todo[$url];
 		switch ($response_status) {
 			case 'success':
-				$results = $this->success_function ? @call_user_func($this->success_function,&$this,&$html,&$headers,&$info) : null;
-				if (is_array($results)) {
+				if ($results = $this->success_function ? call_user_func($this->success_function,&$this,&$html,&$headers,&$info) : null) {
+					if (!is_array($results)) $results = array($results);
 					foreach ($results as $i=>$result) {
 						if (is_resource($this->fp_out)) @fputs($this->fp_out,json_encode($results[$i]) . "\n");
 						else printf("%s\n",trim(print_r($result,true)));
@@ -296,7 +379,7 @@ class Scraper {
 				$this->remove_url($url);
 				break;
 			case 'redirect':
-				if ($this->redirect_function) @call_user_func($this->redirect_function,&$this,&$headers,&$info);
+				if ($this->redirect_function) call_user_func($this->redirect_function,&$this,&$headers,&$info);
 				$this->remove_url($url);
 				break;
 			case 'fail':
@@ -308,6 +391,7 @@ class Scraper {
 				break;
 			default:
 		}
+		/* Useful to stop the script if pcntl extension is not loaded */
 		if (file_exists('.STOP')) {
 			$this->debug('STOP signal received, exiting',1);
 			exit;
