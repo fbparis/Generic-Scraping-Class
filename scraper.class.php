3261d560693d1619a9e8b2f7f65e2015df121e0e
FB <fbparis@gmail.com>
2012-07-12 08:19:41 +0200

    Big commit, new version
    Memory management, user functions for scraps or redirects, all files
    stuff directly managed by the class...
    Less options but more power :)

--- scraper.class.php
+++ scraper.class.php
@@ -26,25 +26,37 @@ class Scraper {
 	public $default_timeout = 30; // in seconds
 	public $debug_level = 0; // 0 = all ; 1 = notices ; 2 = errors
 	public $max_retry = 5; // max retries on 0 and 5xx responses
+	public $max_memory_usage_ratio = 0.8; // if $todo_file is set, new urls will be written in file if memory usage exceeds this ratio
 	
-	public $output_file = ''; // Optional, 1 json encoded object per line will be written
-	public $input_file = ''; // Optional, 1 URL to scrap per line
-	public $errors_file = ''; // Optional, URLS that have not been scraped ; each line is formated as: URL HTTP_CODE
+	/* Public advanced stuff */
+	public $success_codes = array(200);
+	public $fail_codes = array(0,118,204,300,310,500,501,502,503,504,505,507,509);
+	public $redirect_codes = array(301,302,303,307);
 	
 	/* Private internal stuff */
+	protected $input_file = '';
+	protected $output_file = '';
+	protected $todo_file = '';
+	protected $recovery_file = '';
+	protected $errors_file = '';
 	protected $todo = array();  
-	protected $done = false;
+	protected $headers = array();
+	protected $done = true;
 	protected $conns = array();
 	protected $interfaces = array();
 	protected $fp_out = null;
 	protected $fp_in = null;
 	protected $fp_errors = null;
-	protected $timer = 0;		
+	protected $fp_todo = null;
+	protected $timer = 0;
+	protected $memory_limit = 0;
+	protected $todo_swaps = 0;
+	protected $success_function = null;
+	protected $redirect_function = null;
 		
 	/* Special stuff for recovering mode */
 	public $recovery_mode = false;
 	protected $fp_in_offset = 0;
-	protected $recovery_file = null;
 		
 	function __construct() {
 		$this->recovery_file = __FILE__ . '.recover.inc';
@@ -59,6 +71,11 @@ class Scraper {
 				exit;
 			}
 		}
+		$this->memory_limit = intval(ini_get('memory_limit')) * 1024 * 1024;
+		$this->input_file = __FILE__ . '.input';
+		$this->output_file = __FILE__ . '.output';
+		$this->todo_file = __FILE__ . '.todo';
+		$this->errors_file = __FILE__ . '.errors';
 		register_shutdown_function(array($this, '_destruct'));
 		if (function_exists('pcntl_signal')) {
 			$this->debug('System interruptions will be intercepted!',1);	
@@ -71,7 +88,10 @@ class Scraper {
 		if (!$this->done) {
 			if (is_resource($this->fp_out)) @fclose($this->fp_out);
 			if (is_resource($this->fp_errors)) @fclose($this->fp_errors);
+			if (is_resource($this->fp_todo)) @fclose($this->fp_todo);
 			$this->recovery_mode = true;
+			$this->success_function = null;
+			$this->redirect_function = null;
 			foreach ($this->todo as $url=>$status) if ($status > 0) {
 				$this->todo[$url] = 1 - $status;
 			}
@@ -82,7 +102,7 @@ class Scraper {
 				$this->fp_in_offset = ftell($this->fp_in);
 				@fclose($this->fp_in);
 			}
-			$this->fp_in = $this->fp_out = $this->fp_errors = null;
+			$this->fp_in = $this->fp_out = $this->fp_errors = $this->fp_todo = null;
 			if (!@file_put_contents($this->recovery_file,serialize($this))) printf("%s\n",serialize($this));
 			$this->done = true; // prevent multiple executions...
 		}
@@ -99,25 +119,70 @@ class Scraper {
 		return false;
 	}
 	
-	public function add_url($url) {
+	public function add_url($url,$headers=null) {
 		if ($this->recovery_mode) return true;
 		if (array_key_exists($url,$this->todo)) return false;
+		$headers = is_array($headers) ? $headers : null;
+		if (is_resource($this->fp_todo) && (memory_get_peak_usage(true) / $this->memory_limit > $this->max_memory_usage_ratio)) {
+			if (@fputs($this->fp_todo,sprintf("%s %s\n",$url,serialize($headers)))) return true;
+		}
 		$this->todo[$url] = 0;
+		$this->headers[$url] = $headers;
 		return true;
+	}
+	
+	public function remove_url($url) {
+		if (array_key_exists($url,$this->todo)) {
+			unset($this->todo[$url]);
+			unset($this->headers[$url]);
+		}
+	}
+	
+	public function register_success_function($function) {
+		if (function_exists($function)) {
+			$this->success_function = $function;
+			return true;
+		}
+		$this->debug("Success function $function not found!",2);
+		return false;
+	}
+
+	public function register_redirect_function($function) {
+		if (function_exists($function)) {
+			$this->redirect_function = $function;
+			return true;
+		}
+		$this->debug("Redirect function $function not found!",2);
+		return false;
 	}
 	
 	/* Call this method to start scraping */
 	public function run() {
 		if (!count($this->interfaces)) $this->add_interface();
 		$this->timer = $this->recovery_mode ? microtime(true) - $this->timer : microtime(true);
-		if (is_readable($this->input_file)) {
-			$this->fp_in = @fopen($this->input_file,'r');
+		if ($this->fp_in = @fopen($this->input_file,'rb')) {
 			@fseek($this->fp_in,$this->fp_in_offset);
+		} else {
+			if (file_exists($this->input_file)) {
+				$this->debug(sprintf('Input file "%s" not readable! Exiting',$this->input_file),2);
+				exit;
+			}
 		}
-		if ($this->output_file) $this->fp_out = @fopen($this->output_file,$this->recovery_mode ? 'a' : 'w');
-		if ($this->errors_file) $this->fp_errors = @fopen($this->errors_file,$this->recovery_mode ? 'a' : 'w');
+		if (!$this->fp_out = @fopen($this->output_file,$this->recovery_mode ? 'ab' : 'wb')) {
+			$this->debug(sprintf('Output file "%s" not writable! Exiting',$this->output_file),2);
+			exit;
+		}
+		if (!$this->fp_errors = @fopen($this->errors_file,$this->recovery_mode ? 'ab' : 'wb')) {
+			$this->debug(sprintf('Errors file "%s" not writable! Exiting',$this->errors_file),2);
+			exit;
+		}
+		if (!$this->fp_todo = @fopen($this->todo_file,$this->recovery_mode ? 'ab' : 'wb')) {
+			$this->debug(sprintf('Todo file "%s" not writable! Exiting',$this->todo_file),2);
+			exit;
+		}
 		$mh = curl_multi_init();
 		$this->done = count($this->interfaces) == 0;
+		$this->recovery_mode = false;
 		while (!$this->done) {
 			while ($this->assign_conn($mh)); 
 			$status = curl_multi_exec($mh,$active);
@@ -125,6 +190,7 @@ class Scraper {
 			usleep(50);			
 		}
 		curl_multi_close($mh);
+		if (is_resource($this->fp_todo)) @fclose($this->fp_todo);
 		if (is_resource($this->fp_errors)) @fclose($this->fp_errors);
 		if (is_resource($this->fp_out)) @fclose($this->fp_out);
 		if (is_resource($this->fp_in)) @fclose($this->fp_in);
@@ -137,25 +203,55 @@ class Scraper {
 	
 	protected function assign_conn(&$mh) {
 		/* First, look in $todo for a ready to scrap url */
-		foreach ($this->todo as $url=>$status) if ($status <= 0) return $this->add_conn($mh,$url,$status);
+		foreach ($this->todo as $url=>$status) if ($status <= 0) return $this->add_conn($mh,$url,$this->headers[$url],$status);
 		/* If nothing found in $todo, try to get one from the input file */
-		if (is_resource($this->fp_in) && ($url = trim(@fgets($this->fp_in)))) return $this->add_conn($mh,$url,0);
+		if (is_resource($this->fp_in) && ($url = trim(@fgets($this->fp_in)))) {
+			list($url,$headers) = split(' ',$url,2);
+			$headers = @unserialize($headers);
+			return $this->add_conn($mh,$url,$headers,0);
+		}
+		/* If stuffs in todo_file, switch it with input_file */
+		if (is_resource($this->fp_todo)) {
+			clearstatcache();
+			if (filesize($this->todo_file) > 0) {
+				$this->debug('Swapping todo file with input file...');
+				@fclose($this->fp_todo);
+				$this->fp_todo = null;
+				if (is_resource($this->fp_in)) {
+					@fclose($this->fp_in);
+					$this->fp_in = null;
+					if (!rename($this->input_file,$this->input_file . '.' . $this->todo_swaps)) {
+						$this->debug('Unable to rename input file! Exiting',2);
+						exit;
+					}
+					$this->todo_swaps++;
+				}
+				if (!rename($this->todo_file,$this->input_file)) {
+					$this->debug('Unable to rename todo file! Exiting',2);
+					exit;
+				}
+				$this->fp_in = @fopen($this->input_file,'rb');
+				$this->fp_todo = @fopen($this->todo_file,'wb');
+				return $this->assign_conn($mh);
+			}
+		}
 		/* Nothing to scrap, check if done and return false */
 		if (!count($this->todo) && !count($this->conns)) $this->done = true;
 		return false;
 	}
 	
-	protected function add_conn(&$mh,$url,$status=0) {
+	protected function add_conn(&$mh,$url,$headers=null,$status=0) {
 		foreach ($this->interfaces as $k=>$interface) if ($interface->ready()) {
 			// Last used interface is moved to the bottom of the pile 
 			unset($this->interfaces[$k]);
 			$this->interfaces[$k] = $interface;
 			$interface = &$this->interfaces[$k];
 			// Get a connection
-			$ch = $interface->get_conn($url);
+			$ch = $interface->get_conn($url,$this->headers[$url]);
 			$ret = curl_multi_add_handle($mh,$ch);
 			if (0 === $ret) {
 				$this->todo[$url] = 1 - $status;
+				$this->headers[$url] = $headers;
 				$this->conns[$url] = $k;
 				$this->debug(">>> $url");
 				return true;
@@ -167,55 +263,55 @@ class Scraper {
 		}
 		/* no available connection */
 		$this->todo[$url] = $status;
+		$this->headers[$url] = $headers;
 		return false;
 	}
 	
 	protected function get_status($http_code) {
-		if ($http_code == 200) return 'success';
-		if ($http_code == 0) return 'fail';
-		if ($http_code >= 500) return 'fail';
+		if (in_array($http_code,$this->success_codes)) return 'success';
+		if (in_array($http_code,$this->fail_codes)) return 'fail';
+		if (in_array($http_code,$this->redirect_codes)) return 'redirect';
 		return null;
 	}
 	
 	protected function exec_conn(&$mh,&$ch) {
 		$info = curl_getinfo($ch);
-		$html = curl_multi_getcontent($ch);
+		list($html,$headers) = split("\r\n\r\n",curl_multi_getcontent($ch),2);
 		curl_multi_remove_handle($mh,$ch);
-		$this->interfaces[$this->conns[$info['url']]]->close_conn($ch,$this->get_status($info['http_code']));
+		$response_status = $this->get_status($info['http_code']);
+		$this->interfaces[$this->conns[$info['url']]]->close_conn($ch,$response_status);
 		unset($this->conns[$info['url']]);
 		$this->debug(sprintf("<<< %s (%d)",$info['url'],$info['http_code']));
 		$status = $this->todo[$info['url']];
-		$ret = true;
-		switch ($info['http_code']) {
-			case 200:
-				
-				/* Add code to add other url(s) in $todo here */
-				
-				if ($results = $this->extract_datas($html)) {
+		switch ($response_status) {
+			case 'success':
+				$results = $this->success_function ? @call_user_func($this->success_function,&$this,&$html,&$headers,&$info) : null;
+				if (is_array($results)) {
 					foreach ($results as $i=>$result) {
 						if (is_resource($this->fp_out)) @fputs($this->fp_out,json_encode($results[$i]) . "\n");
 						else printf("%s\n",trim(print_r($result,true)));
 					}
 				}
+				$this->remove_url($url);
+				break;
+			case 'redirect':
+				if ($this->redirect_function) @call_user_func($this->redirect_function,&$this,&$headers,&$info);
+				$this->remove_url($url);
+				break;
+			case 'fail':
+				if ($status < $this->max_retry) $this->todo[$info['url']] = -$status;
+				else {
+					if (is_resource($this->fp_errors)) @fputs($this->fp_errors,sprintf("%s %d\n",$info['url'],$info['http_code']));
+					$this->remove_url($url);
+				}
 				break;
 			default:
-				$ret = false;
-				$this->debug(sprintf("Error parsing %s (%d)",$info['url'],$info['http_code']),2);
-				if ($this->get_status($info['http_code']) == 'fail') {
-					if ($status < $this->max_retry) $this->todo[$info['url']] = -$status;
-					else if (is_resource($this->fp_errors)) @fputs($this->fp_errors,sprintf("%s %d\n",$info['url'],$info['http_code']));
-				} 
 		}
-		unset($this->todo[$info['url']]);
-		return $ret;
-	}
-	
-	protected function extract_datas(&$html) {
-		$results = array();
-		
-		/* Add code to extract datas here */
-		
-		return count($results) ? $results : false;
+		if (!$response_status == 'success') return true;
+		else {
+			$this->debug(sprintf("*** %s has returned a %d http code",$info['url'],$info['http_code']),1);
+			return false;
+		}
 	}
 }
 
@@ -248,11 +344,13 @@ class ScraperInterface {
 		return ($this->conns < $this->current_max_conns) && (($this->sleep_delay == 0) || (((microtime(true) - $this->last_conn)) >= $this->sleep_delay));
 	}
 	
-	public function get_conn($url) {
+	public function get_conn($url,$headers=null) {
 		$ch = curl_init($url);
 		if ($this->ip) curl_setopt($ch,CURLOPT_INTERFACE,$this->ip);
 		$ua = is_array($this->user_agent) && count($this->user_agent) ? $this->user_agent[mt_rand(0,count($this->user_agent) - 1)] : $this->user_agent;
 		if (is_string($ua)) curl_setopt($ch,CURLOPT_USERAGENT,$ua);
+		if (is_array($headers) && count($headers)) curl_setopt($ch,CURLOPT_HTTPHEADER,$headers);
+		curl_setopt($ch,CURLOPT_HEADER,true);
 		curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
 		curl_setopt($ch,CURLOPT_TIMEOUT,$this->timeout);
 		curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
@@ -268,36 +366,26 @@ class ScraperInterface {
 		if (!$this->auto_adjust_speed) return true;
 		switch ($status) {
 			case 'fail':
-				return $this->trigger_fail();
+				if (!$this->failed) {
+					$this->failed = true;
+					break;
+				}
+				if ($this->current_max_conns > 1) {
+					$this->current_max_conns--;
+				} else {
+					if ($this->sleep_delay == 0) $this->sleep_delay = 1;
+					else $this->sleep_delay = min($this->max_sleep_delay,$sleep_delay * 2);
+				}
+				break;
 			case 'success':
-				return $this->trigger_success();
-			default:
-				return true;
+				$this->failed = false;
+				if ($this->sleep_delay > 0) {
+					$this->sleep_delay = round($this->sleep_delay / 3);
+				} else if ($this->current_max_conns < $this->max_conns) {
+					$this->current_max_conns++;
+				}
+				break;
 		}
-	}
-	
-	protected function trigger_fail() {
-		if (!$this->failed) {
-			$this->failed = true;
-			return true;
-		}
-		if ($this->current_max_conns > 1) {
-			$this->current_max_conns--;
-		} else {
-			if ($this->sleep_delay == 0) $this->sleep_delay = 1;
-			else $this->sleep_delay = min($this->max_sleep_delay,$sleep_delay * 2);
-		}
-		return true;
-	}
-	
-	protected function trigger_success() {
-		$this->failed = false;
-		if ($this->sleep_delay > 0) {
-			$this->sleep_delay = round($this->sleep_delay / 3);
-		} else if ($this->current_max_conns < $this->max_conns) {
-			$this->current_max_conns++;
-		}
-		return true;
 	}
 }
 
